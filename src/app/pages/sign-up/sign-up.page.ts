7265cf65e6b8eba2698676bd1ab6f11784a7b90c
sooraj_pn <you@example.com>
2019-09-02 11:04:05 +0530

    menu added to the ride page

--- src/app/pages/sign-up/sign-up.page.ts
+++ src/app/pages/sign-up/sign-up.page.ts
@@ -75,6 +75,7 @@ export class SignUpPage implements OnInit {
   }
 
   validateEmail() {
+    // tslint:disable-next-line: max-line-length
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (!re.test(String(this.email).toLowerCase())) {
